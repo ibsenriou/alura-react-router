c11418067e0f9e42fea5e3ae0d9943e002d4aeb3
Ibsen Gabriel Santos <ibsenriou@hotmail.com>
2022-07-04 18:16:05 -0300

    Conclusão da Aula 4: Router DOM Hooks.

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,5 +1,7 @@
 import cardapio from 'data/cardapio.json';
 import styles from './Inicio.module.scss';
+import stylesTema from 'styles/Tema.module.scss';
+import nossaCasa from 'assets/nossa_casa.png';
 
 export default function Inicio() {
 
@@ -8,7 +10,7 @@ export default function Inicio() {
     sort(() => 0.5 - Math.random()).splice(0, 3);
   return (
     <section>
-      <h3 className={styles.titulo}>
+      <h3 className={stylesTema.titulo}>
         Recomendações da cozinha
       </h3>
       <div className={styles.recomendados}>
@@ -22,6 +24,13 @@ export default function Inicio() {
             </div>
           </div>
         ))}
+      </div>
+      <h3 className={stylesTema.titulo}> Nossa casa</h3>
+      <div className={styles.nossaCasa}>
+        <img src={nossaCasa} alt="Casa do aluroni" />
+        <div className={styles.nossaCasa__endereco}>
+          Rua Vergueiro, 3185 <br /> <br /> Vila Mariana - SP
+        </div>
       </div>
     </section>
   );
