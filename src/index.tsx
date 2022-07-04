8c71096ccd000ec5bbbc18908090f922e82b9625
Ibsen Gabriel Santos <ibsenriou@hotmail.com>
2022-07-04 15:49:26 -0300

    Conclusão da Aula 3: Criando Rotas.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Router from './routes';
 import 'normalize.css';
 import './index.css';
-import Cardapio from './pages/Cardapio';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Cardapio />
+    <Router />
   </React.StrictMode>,
   document.getElementById('root')
 );
